f599c3d69ab53e1514b8d42ca2c6c42e4a906821
Michael Zoellner <michael.zoellner@mailbox.org>
2020-03-17 14:45:20 +1300

    updating reflect-metadata to the latest version
    explicitly importing reflect-metadata, so it can be used
    adding reflect-metadata to peer dependencies

--- projects/ensure/src/value/value.ts
+++ projects/ensure/src/value/value.ts
@@ -1,5 +1,4 @@
 // Dependencies:
-import { isNotNull } from '../guards/not-null';
 import { Guard } from '../guard/guard';
 
 // Constants:
@@ -14,7 +13,7 @@ export function Value (...guards: Array<Guard<any> | Array<Guard<any>>>): Proper
     return (target: any, propertyKey: string) => {
         Object.defineProperty(target, propertyKey, {
             get: function () {
-                const value = (Reflect as any).getMetadata(`${VALUE_KEY}${propertyKey}`, this);
+                const value = Reflect.getMetadata(`${VALUE_KEY}${propertyKey}`, this);
                 getters.forEach(getter => {
                     getter.call(this, value, propertyKey);
                 });
@@ -32,7 +31,7 @@ export function Value (...guards: Array<Guard<any> | Array<Guard<any>>>): Proper
                         castValue = setter.call(this, castValue, propertyKey);
                     });
                 }
-                (Reflect as any).defineMetadata(`${VALUE_KEY}${propertyKey}`, castValue, this);
+                Reflect.defineMetadata(`${VALUE_KEY}${propertyKey}`, castValue, this);
             }
         });
     };
